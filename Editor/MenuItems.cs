60369e5285c959574e8774c0cbba9dd8a379912c
Thomas ICHÉ <peeweek@gmail.com>
2019-01-16 08:38:13 +0100

    Added Icons + Started working on Scene Logic Editor

---
+++ Editor/MenuItems.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace GameplayIngredients.Editor
+{
+    static class MenuItems
+    {
+        [MenuItem("Edit/Select None &D", priority = 149)]
+        static void UnselectAll()
+        {
+            Selection.activeObject = null;
+        }
+
+
+        static readonly string helperPreferenceName = "GameplayIngredients.toggleIngredientHelpers";
+        [MenuItem("Edit/Gameplay Ingredients/Toggle Helpers", priority = 117)]
+        static void ToggleIngredientHelpers()
+        {
+            bool value = EditorPrefs.GetBool(helperPreferenceName, false);
+            value = !value;
+            EditorPrefs.SetBool(helperPreferenceName, value);
+
+        }
+
+        [MenuItem("Edit/Gameplay Ingredients/Toggle Helpers", true, 117)]
+        static bool ToggleIngredientHelpersValidation()
+        {
+            Menu.SetChecked("Edit/Gameplay Ingredients/Toggle Helpers", EditorPrefs.GetBool(helperPreferenceName, false));
+            return true;
+        }
+
+    }
+}
+
---
+++ Editor/SceneLogicEditor/SceneLogicEditor.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using GraphProcessor;
+using System.Reflection;
+using GameplayIngredients.Hooks;
+using GameplayIngredients.Logic;
+using GameplayIngredients.Actions;
+
+public class SceneLogicEditor : BaseGraphWindow
+{
+    [MenuItem("Window/Gameplay Ingredients/Scene Logic")]
+    static void OpenLogicEditor()
+    {
+        GetWindow<SceneLogicEditor>();
+    }
+
+    protected override void Initialize(BaseGraph graph)
+    {
+        titleContent = Contents.title;
+
+    }
+
+    static class Contents
+    {
+        public static readonly GUIContent title = new GUIContent("Scene Logic");
+    }
+
+    public List<Type> hookTypes;
+    public List<Type> logicTypes;
+    public List<Type> actionTypes;
+
+    void PopulateAllTypes()
+    {
+        var result = new List<Type>();
+        Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+
+        var hookBase = typeof(HookBase);
+        var logicBase = typeof(LogicBase);
+        var actionBase = typeof(ActionBase);
+
+        hookTypes = new List<Type>();
+        logicTypes = new List<Type>();
+        actionTypes = new List<Type>();
+
+        foreach (var assemly in assemblies)
+        {
+            Type[] types = assemly.GetTypes();
+            foreach (var type in types)
+            {
+                if (hookBase.IsAssignableFrom(type) && !type.IsAbstract)
+                {
+                    hookTypes.Add(type);
+                }
+                else if (logicBase.IsAssignableFrom(type) && !type.IsAbstract)
+                {
+                    logicTypes.Add(type);
+                }
+                else if (hookBase.IsAssignableFrom(type) && !type.IsAbstract)
+                {
+                    actionTypes.Add(type);
+                }
+            }
+        }
+    }
+}
