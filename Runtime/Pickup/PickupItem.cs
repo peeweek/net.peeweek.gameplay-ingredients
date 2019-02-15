e6577748dc5f39e834f66eff05c7f2be79597c8a
Thomas ICHÉ <thomasi@unity3d.com>
2019-02-15 15:51:59 +0100

    Pickup Item display + base behaviour

--- Runtime/Pickup/PickupItem.cs
+++ Runtime/Pickup/PickupItem.cs
@@ -4,9 +4,22 @@ using UnityEngine;
 
 namespace GameplayIngredients.Pickup
 {
+    [RequireComponent(typeof(Collider))]
     public class PickupItem : MonoBehaviour
     {
         public PickupEffectBase[] effects { get { return GetComponents<PickupEffectBase>();  } }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            var owner = other.gameObject.GetComponent<PickupOwnerBase>();
+            if(owner != null)
+            {
+                if(owner.PickUp(this))
+                {
+                    Destroy(this.gameObject);
+                }
+            }
+        }
     }
 }
 
