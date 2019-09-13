a8a8789db65e0c4c53ee7f5025d1eb6a534455e1
Laurent <laurenth@unity3d.com>
2019-09-13 14:56:58 +0200

    add joint break event

---
+++ Runtime/LevelScripting/Events/OnJointBreakEvent.cs
@@ -0,0 +1,17 @@
+﻿using NaughtyAttributes;
+using UnityEngine;
+
+namespace GameplayIngredients.Events
+{
+    [RequireComponent(typeof(Joint))]
+    public class OnJointBreakEvent : EventBase
+    {
+        [ReorderableList]
+        public Callable[] onJointBreak;
+
+        private void OnJointBreak(Collider other)
+        {
+            Callable.Call(onJointBreak, other.gameObject);
+        }
+    }
+}
