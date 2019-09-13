0e833a726abb9c5e0db65821cbfd69dcef23b261
Laurent <laurenth@unity3d.com>
2019-09-13 11:41:27 +0200

    add flip flop logic

---
+++ Runtime/LevelScripting/Logic/FlipFlopLogic.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using NaughtyAttributes;
+
+namespace GameplayIngredients.Logic
+{
+    public class FlipFlopLogic : LogicBase
+    {
+        [ReorderableList]
+        public Callable[] OnConditionValid;
+
+        [ReorderableList]
+        public Callable[] OnConditionInvalid;
+
+        private bool condition = true;
+
+        public override void Execute(GameObject instigator = null)
+        {
+            if (condition)
+            {
+                Callable.Call(OnConditionValid, instigator);
+                condition = false;
+            }
+            else
+            {
+                Callable.Call(OnConditionInvalid, instigator);
+                condition = true;
+            }
+        }
+    }
+}
