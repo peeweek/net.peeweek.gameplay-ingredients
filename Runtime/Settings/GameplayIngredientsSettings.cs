6831799ce3617eb16f2cb1583628903d0cbf903e
Thomas ICHÉ <peeweek@gmail.com>
2019-09-18 13:59:24 +0200

    Added OnUpdate Event + Safety Checks

--- Runtime/Settings/GameplayIngredientsSettings.cs
+++ Runtime/Settings/GameplayIngredientsSettings.cs
@@ -24,7 +24,7 @@ namespace GameplayIngredients
         [BoxGroup("Callables")]
         [SerializeField, InfoBox("Verbose Calls enable logging at runtime, this can lead to performance drop, use only when debugging.", InfoBoxType.Warning, "m_VerboseCalls")]
         private bool m_VerboseCalls = false;
-        [SerializeField, InfoBox("Per-update calls should be avoided. Enable if strictly necessary.", InfoBoxType.Warning, "m_AllowUpdateCalls")]
+        [SerializeField, InfoBox("Per-update calls should be avoided due to high performance impact. Enable and use with care, only if strictly necessary.", InfoBoxType.Warning, "m_AllowUpdateCalls")]
         private bool m_AllowUpdateCalls = false;
 
         const string kAssetName = "GameplayIngredientsSettings";
