eda53e9d86a8061773959a5bbfe418351c92ced5
Thomas ICHÉ <peeweek@gmail.com>
2021-02-28 07:06:28 +0100

    Caching results

--- Editor/CheckWindow/CheckWindow.cs
+++ Editor/CheckWindow/CheckWindow.cs
@@ -56,8 +56,13 @@ namespace GameplayIngredients.Editor
                 EditorPrefs.SetBool(kShowIgnoredPreference, value); 
             }
         }
+
         Vector2 Scroll = new Vector2();
         Dictionary<Check, bool> s_CheckStates;
+
+        int noticeCount;
+        int warningCount;
+        int errorCount;
 
         enum SortMode
         {
@@ -185,9 +190,9 @@ namespace GameplayIngredients.Editor
                 filterString = EditorGUILayout.DelayedTextField(filterString, EditorStyles.toolbarSearchField, GUILayout.Width(180));
 
                 showIgnored = GUILayout.Toggle(showIgnored, "Ignored", EditorStyles.toolbarButton);
-                showNotice = GUILayout.Toggle(showNotice, new GUIContent(m_Results.Where(o => o.result == CheckResult.Result.Notice).Count().ToString(), CheckResult.GetIcon(CheckResult.Result.Notice)), EditorStyles.toolbarButton);
-                showWarning = GUILayout.Toggle(showWarning, new GUIContent(m_Results.Where(o => o.result == CheckResult.Result.Warning).Count().ToString(), CheckResult.GetIcon(CheckResult.Result.Warning)), EditorStyles.toolbarButton);
-                showError = GUILayout.Toggle(showError, new GUIContent(m_Results.Where(o => o.result == CheckResult.Result.Failed).Count().ToString(), CheckResult.GetIcon(CheckResult.Result.Failed)), EditorStyles.toolbarButton);
+                showNotice = GUILayout.Toggle(showNotice, new GUIContent(noticeCount.ToString(), CheckResult.GetIcon(CheckResult.Result.Notice)), EditorStyles.toolbarButton);
+                showWarning = GUILayout.Toggle(showWarning, new GUIContent(warningCount.ToString(), CheckResult.GetIcon(CheckResult.Result.Warning)), EditorStyles.toolbarButton);
+                showError = GUILayout.Toggle(showError, new GUIContent(errorCount.ToString(), CheckResult.GetIcon(CheckResult.Result.Failed)), EditorStyles.toolbarButton);
 
             }
             using(new GUILayout.VerticalScope())
@@ -199,8 +204,7 @@ namespace GameplayIngredients.Editor
                     SortButton("Object", SortMode.ObjectName, GUILayout.Width(128));
                     SortButton("Message", SortMode.Message, GUILayout.ExpandWidth(true));
                     SortButton("Resolution", SortMode.Resolution, GUILayout.Width(128));
-                    if(showIgnored)
-                        SortButton("Ignored", SortMode.Ignored, GUILayout.Width(64));
+                    SortButton("Ignored", SortMode.Ignored, GUILayout.Width(64));
                     GUILayout.Space(12);
                 }
 
@@ -211,7 +215,6 @@ namespace GameplayIngredients.Editor
                     bool odd = true;
                     foreach (var result in m_Results)
                     {
-
                         if (!showIgnored && IsIgnored(result) || result.mainObject == null)
                             continue;
 
@@ -244,17 +247,14 @@ namespace GameplayIngredients.Editor
 
                             EditorGUI.EndDisabledGroup();
 
-                            if(showIgnored)
+                            GUILayout.Space(18);
+                            EditorGUI.BeginChangeCheck();
+                            var ignored = GUILayout.Toggle(IsIgnored(result),"", EditorStyles.toggle ,GUILayout.Width(24));
+                            if (EditorGUI.EndChangeCheck())
                             {
-                                GUILayout.Space(18);
-                                EditorGUI.BeginChangeCheck();
-                                var ignored = GUILayout.Toggle(IsIgnored(result),"", EditorStyles.toggle ,GUILayout.Width(24));
-                                if (EditorGUI.EndChangeCheck())
-                                {
-                                    SetIgnored(result, ignored);
-                                }
-                                GUILayout.Space(18);
+                                SetIgnored(result, ignored);
                             }
+                            GUILayout.Space(18);
 
                         }
                     }
@@ -380,9 +380,15 @@ namespace GameplayIngredients.Editor
                 m_Results = results;
 
             sortMode = SortMode.None;
+
+            noticeCount = m_Results.Where(o => o.result == CheckResult.Result.Notice).Count();
+            warningCount = m_Results.Where(o => o.result == CheckResult.Result.Warning).Count();
+            errorCount = m_Results.Where(o => o.result == CheckResult.Result.Failed).Count();
+
             BuildIgnoredList();
             Repaint();
         }
+
 
         static class Styles
         {
