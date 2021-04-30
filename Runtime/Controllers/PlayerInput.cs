53a5456bd01d6edd37d506b8a15aa08307da1b3b
Thomas ICHÉ <peeweek@gmail.com>
2021-04-30 09:34:17 +0200

    Factorized Player Input for both systems

--- Runtime/Controllers/PlayerInput.cs
+++ Runtime/Controllers/PlayerInput.cs
@@ -1,8 +1,12 @@
 using UnityEngine;
+#if ENABLE_INPUT_SYSTEM
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+#endif
 
 namespace GameplayIngredients.Controllers
 {
-    public abstract class PlayerInput : MonoBehaviour
+    public abstract class PlayerInput : GameplayIngredientsBehaviour
     {
         public enum ButtonState
         {
@@ -17,9 +21,36 @@ namespace GameplayIngredients.Controllers
         public abstract ButtonState Jump { get; }
 
         public abstract void UpdateInput();
-
-        protected static ButtonState GetButtonState(string Button)
+        
+#if ENABLE_INPUT_SYSTEM
+        protected static ButtonState GetButtonControlState(ButtonControl bc)
         {
+            if(bc == null)
+            {
+                return ButtonState.Released;
+            }
+
+            if (bc.isPressed)
+            {
+                if(bc.wasPressedThisFrame)
+                    return ButtonState.JustPressed;
+                else
+                    return ButtonState.Pressed;
+            }
+            else
+            {
+                if(bc.wasReleasedThisFrame)
+                    return ButtonState.JustReleased;
+                else
+                    return ButtonState.Released;
+            }
+        }
+#endif
+
+
+#if ENABLE_LEGACY_INPUT_MANAGER
+        protected static ButtonState GetButtonState(string Button)
+        {         
             if (Input.GetButton(Button))
             {
                 if (Input.GetButtonDown(Button))
@@ -36,5 +67,6 @@ namespace GameplayIngredients.Controllers
 
             }
         }
+#endif
     }
 }
