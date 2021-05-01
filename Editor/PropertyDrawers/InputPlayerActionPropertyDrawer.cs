b21658872584d22fadd2be8ca77c408617528b5e
Thomas ICHÉ <peeweek@gmail.com>
2021-05-01 06:42:15 +0200

    Input Player Action property drawer

--- Editor/PropertyDrawers/InputPlayerActionPropertyDrawer.cs
+++ Editor/PropertyDrawers/InputPlayerActionPropertyDrawer.cs
@@ -18,15 +18,15 @@ namespace GameplayIngredients.Editor
 
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            var asset = property.FindPropertyRelative("playerInput");
+            var playerInput = property.FindPropertyRelative("playerInput");
             var path = property.FindPropertyRelative("path");
 
             position.height = EditorGUIUtility.singleLineHeight;
-            EditorGUI.PropertyField(position, asset, new GUIContent("Player Input"));
+            EditorGUI.PropertyField(position, playerInput, new GUIContent("Player Input"));
             EditorGUI.indentLevel++;
             position.y += EditorGUIUtility.singleLineHeight + 2;
             
-            var paths = GetPaths(asset.objectReferenceValue as InputActionAsset);
+            var paths = GetPaths(playerInput.objectReferenceValue as PlayerInput);
             int selected = paths.IndexOf(path.stringValue);
             selected = EditorGUI.Popup(position, "Action", selected, paths);
             if(GUI.changed)
@@ -39,14 +39,14 @@ namespace GameplayIngredients.Editor
             EditorGUI.indentLevel--;
         }
 
-        string[] GetPaths(InputActionAsset asset)
+        string[] GetPaths(PlayerInput playerInput)
         {
-            if (asset == null)
+            if (playerInput == null || playerInput.actions == null)
                 return new string[0];
 
             List<string> paths = new List<string>();
 
-            foreach(var map in asset.actionMaps)
+            foreach(var map in playerInput.actions.actionMaps)
             {
                 if (map == null) continue;
                 foreach(var action in map.actions)
