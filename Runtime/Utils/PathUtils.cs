b1c8997427738e72533d307e106b67834633f7bf
peppemas <giuseppe.mastrangelo@outlook.com>
2021-11-20 02:46:52 +0100

    Added a new search method for manager prefabs: Assets/Resources/<SceneName>

--- Runtime/Utils/PathUtils.cs
+++ Runtime/Utils/PathUtils.cs
@@ -7,12 +7,21 @@ namespace GameplayIngredients.Utils
     {
         public static string GetCurrentSceneResourcesPath()
         {
-            string path = SceneManager.GetActiveScene().path
+            // Method 1: Search into Assets/Resources/<SceneName>
+            string path = "Assets/Resources/" + SceneManager.GetActiveScene().name;
+            if (AssetDatabase.IsValidFolder(path))
+            {
+                return path;
+            }
+
+            // Method 2: Search into the current scene path the directory "Resources"
+            path = SceneManager.GetActiveScene().path
                 .Replace(SceneManager.GetActiveScene().name + ".unity", "Resources");
             if (AssetDatabase.IsValidFolder(path))
             {
                 return path; 
             }
+            
             return null;
         }
     }
